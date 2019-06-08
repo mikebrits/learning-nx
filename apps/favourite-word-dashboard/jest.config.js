1287f65c6f404cd86705d826bf627020516ee775
Mike Brits <mike.brits@gmail.com>
2019-06-08 07:40:34 +0100

    Initial Commit

---
+++ apps/favourite-word-dashboard/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'favourite-word-dashboard',
+  preset: '../../jest.config.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/apps/favourite-word-dashboard'
+};
